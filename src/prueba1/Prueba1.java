d799c252fa672c41e59ba3b966f83a52a1fa7731
Alfonso <Alfonso@Alfonso-PC>
2015-02-15 20:17:59 -0600

    new commit

--- src/prueba1/Prueba1.java
+++ src/prueba1/Prueba1.java
@@ -17,6 +17,8 @@ public class Prueba1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HOLA WORLD");
+        System.out.println("Mejora");
+            
         
     }
     
